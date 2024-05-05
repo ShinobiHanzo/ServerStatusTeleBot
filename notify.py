885d750469905c158c09f55098c2c6ac25f7fb1e
ShinobiHanzo <81740616+ShinobiHanzo@users.noreply.github.com>
2024-05-05 15:44:12 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,10 +1,6 @@
 import requests, time, datetime, subprocess
 from data import TOKEN, CHAT_ID
 
-
-import subprocess
-import requests
-
 # Telegram bot token
 #TOKEN = BotToken
 
@@ -19,14 +15,24 @@ def send_message(message):
 
 # Function to notify about SSH login
 def notify_ssh_login(username, ip_address):
-    message = f'New SSH login:\nUsername: {username}\nIP Address: {ip_address}'
+    message = f'New SSH login:\nUser_IP: {username}\nPort: {ip_address}'
     send_message(message)
+
+def convert_timestamp(timestamp):
+    try:
+        return int(time.mktime(time.strptime(timestamp, "%b %d %H:%M:%S")))
+    except ValueError:
+        return None
 
 # Function to continuously monitor SSH login events
 def monitor_ssh_logins():
     # Command to monitor SSH login events in real-time
-    ssh_process = subprocess.Popen(['journalctl', '-f', '-n', '0', '_SYSTEMD_UNIT=sshd.service'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    
+    command = ['journalctl', '-u', 'ssh.service', '--since', 'today', '--follow']
+    ssh_process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    # Set current time
+    current_time = time.time()
+
     # Continuously monitor SSH login events
     while True:
         line = ssh_process.stdout.readline().strip()
@@ -34,10 +40,14 @@ def monitor_ssh_logins():
             # Parse the line to extract SSH login details
             if 'Accepted password' in line:
                 parts = line.split()
-                username = parts[10]
-                ip_address = parts[12]
-                # Notify about SSH login
-                notify_ssh_login(username, ip_address)
+                timestamp_str = f"{parts[0]} {parts[1]} {parts[2]} {parts[3]}"
+                timestamp_epoch = convert_timestamp(timestamp_str)
+                # Check if timestamp is valid and not older than 2 hours
+                if timestamp_epoch is not None and current_time - timestamp_epoch <= 7200:
+                    username = parts[10]
+                    ip_address = parts[12]
+                    # Notify about SSH login
+                    notify_ssh_login(username, ip_address)
 
 
 def notify_boot():
